3fd76b1a7b0f9ee73db32a9262d04db85737200a
jackdonnellan161 <jackdonnellan161@gmail.com>
2017-06-27 17:05:39 -0400

    First part done, bonus tbd in seperate file.

--- shakeMonk/shakeMonk.py
+++ shakeMonk/shakeMonk.py
@@ -22,14 +22,34 @@ def check(generated,goal):
 		while ii<total:
 			if generated[ii] == goal[ii]:
 				count = count+1
-				print(ii+1)
+#				print(ii+1)
 			ii = ii+1
-	print(count, total)
-	
-	return 0
+#	print(count,total)	
+	score = count/total
+	flag = int(count/total)
+	group = [score,flag]
+	return group
 
-goal = "methinks it is like a weasel"
-list = generate(len(goal))
-print(list)
-print(goal)
-check(list,goal)
+def looper(most):
+	count = 1
+	goal = "methinks it is like a weasel"
+	generated = generate(len(goal))
+	bestString = ''
+	bestScore = 0
+	end = [bestScore,bestString]
+	while check(generated,goal)[1] != 1:
+		generated  = generate(len(goal))
+		count = count + 1
+		if check(generated,goal)[0] > bestScore:
+			bestString = generated
+			bestScore = check(generated,goal)[0]
+			end = [bestScore,bestString]
+		if count%100000 == 0:
+			print(count,end,sep=": ")
+		if count >= most:
+			raise RuntimeError("Looping for too long.")
+	return end
+
+most = int(input("Maximum number of iterations: "))
+looper(most)
+
