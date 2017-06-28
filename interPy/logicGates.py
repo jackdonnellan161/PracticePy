03e2be2078ab0c63ec97c2cd4e7c23acaf6762f3
jackdonnellan161 <jackdonnellan161@gmail.com>
2017-06-28 19:29:12 -0400

    Gates done, next is connector

--- interPy/logicGates.py
+++ interPy/logicGates.py
@@ -1,11 +1,62 @@
 #! /usr/bin/env python3
 
-class logicGates:
+class LogicGate:
 	def __init__(self,n):
 		self.label = n
 		self.output = None
-	def getLabel(self)
+	def getLabel(self):
 		return self.label
 	def getOutput(self):
-		self.output = self.performLogicGate
+		self.output = self.performGateLogic()
 		return self.output
+
+class BinaryGate(LogicGate):
+	def __init__(self,n):
+		LogicGate.__init__(self,n)
+		self.pin = None
+	def getPinA(self):
+		return int(input("Enter Pin A input for gate "+ self.getLabel()+"-->"))
+	def getPinB(self):
+		return int(input("Enter Pin B input for gate "+ self.getLabel()+"-->"))
+
+class UnaryGate(LogicGate):
+	def __init__(self,n):
+		LogicGate.__init__(self,n)
+		self.pin = None
+	def getPin(self):
+		return int(input("Enter Pin input for gate "+ self.getLabel()+"-->"))		
+
+class AndGate(BinaryGate):
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+        	return 1
+        else:
+        	return 0
+
+class OrGate(BinaryGate):
+	def __init__(self,n):
+		BinaryGate.__init__(self,n)
+	def performGateLogic(self):
+		a = self.getPinA()
+		b = self.getPinB()
+		if a == 1 or b == 1:
+			return 1
+		else:
+			return 0
+class NotGate(UnaryGate):
+	def __init__(self,n):
+		UnaryGate.__init__(self,n)
+	def performGateLogic(self):
+		a = self.getPin()
+		if a ==1:
+			return 0
+		else:
+			return 1
+
+
+g1 = NotGate("G1")
+print(g1.getOutput())
