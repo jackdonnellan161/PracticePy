e5ca5cfb07c8614d606ff62f7b9316f1d6098071
jackdonnellan161 <jackdonnellan161@gmail.com>
2017-06-29 15:20:26 -0400

    Exercises 1-6 done

--- interPy/fracClass.py
+++ interPy/fracClass.py
@@ -4,30 +4,32 @@ class Fraction:
 	def __init__(self,top,bottom):
 		if bottom == 0:
 			raise RuntimeError("Denominator cannot be zero")
-		self.num = top
-		self.den = bottom
+		if top % 1 != 0 or bottom %1 != 0:
+			raise RuntimeError("Both numerator and denominator must be integers")
+		if bottom<0:
+			bottom = -bottom
+			top = -top
+		fac = gcd(top,bottom)
+		self.num = top//fac
+		self.den = bottom//fac
 	def __str__(self):
 		return str(self.num)+"/"+str(self.den)
 	def __add__(self,otherFrac):
 		newNum = (self.num*otherFrac.den) + (otherFrac.num*self.den)
 		newDen = self.den*otherFrac.den
-		fac = gcd(newNum,newDen)
-		return Fraction(newNum//fac,newDen//fac)
+		return Fraction(newNum,newDen)
 	def __sub__(self,otherFrac):
 		newNum = (self.num*otherFrac.den)-(self.den*otherFrac.num)
 		newDen = self.den*otherFrac.den
-		fac = gcd(newNum,newDen)
-		return Fraction(newNum//fac,newDen//fac)
+		return Fraction(newNum,newDen)
 	def __mul__(self,otherFrac):
 		newNum = self.num*otherFrac.num
 		newDen = self.den*otherFrac.den
-		fac = gcd(newNum,newDen)
-		return Fraction(newNum//fac,newDen//fac)
+		return Fraction(newNum,newDen)
 	def __truediv__(self,otherFrac):
 		newNum = self.num*otherFrac.den
 		newDen = self.den*otherFrac.num
-		fac = gcd(newNum,newDen)
-		return Fraction(newNum//fac,newDen//fac)	
+		return Fraction(newNum,newDen)	
 	def __eq__(self,otherFrac):
 		return self.num*otherFrac.den == otherFrac.num*self.den
 	def __ne__(self,otherFrac):
@@ -42,26 +44,30 @@ class Fraction:
 		return self.num*otherFrac.den <= otherFrac.num*self.den
 	def show(self):
 		print(self.num,"/",self.den)
+	def getNum(self):
+		return self.num
+	def getDen(self):
+		return self.den
+
 def gcd(m,n):
 	num = 0
 	if n<0:
 		n = -n
+	if m<0:
+		m = -m
 	if m%n == 0:
 		num = n
 	else:
 		num = gcd(n,m%n)
 	return num
 
-myF = Fraction(3,5)
-otherF = Fraction(4,5)
-thirdF = Fraction(30,50)
-#myF.show()
-#print(myF,"works")
-#print(myF==otherF)
-#print(gcd(20,10))
-#print(myF==thirdF)
-#print(Fraction(1,2)-Fraction(1,3))
-#print(Fraction(1,2)-Fraction(8,5))
-print(Fraction(1,10)/Fraction(1,15))
-print(Fraction(5,3)/Fraction(3,10))
+def main():	
+	myF = Fraction(3,5)
+	otherF = Fraction(4,5)
+	thirdF = Fraction(30,50)
+	print(Fraction(1,10)+Fraction(1,-15))
+	print(Fraction(5,3)/Fraction(3,10))
+
+
+main()
 
