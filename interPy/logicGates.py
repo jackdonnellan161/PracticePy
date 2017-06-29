c37be10e419497970a22d6b19fb12cb4a2c24957
jackdonnellan161 <jackdonnellan161@gmail.com>
2017-06-29 15:00:18 -0400

    Connectors complete, working on exercises

--- interPy/logicGates.py
+++ interPy/logicGates.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python3
-
 class LogicGate:
 	def __init__(self,n):
-		self.label = n
+		self.name = n
 		self.output = None
-	def getLabel(self):
-		return self.label
+	def getName(self):
+		return self.name
 	def getOutput(self):
 		self.output = self.performGateLogic()
 		return self.output
@@ -13,50 +12,144 @@ class LogicGate:
 class BinaryGate(LogicGate):
 	def __init__(self,n):
 		LogicGate.__init__(self,n)
-		self.pin = None
+		self.pinA = None
+		self.pinB = None
 	def getPinA(self):
-		return int(input("Enter Pin A input for gate "+ self.getLabel()+"-->"))
+		if self.pinA == None:
+			return int(input("Enter Pin A input for gate "+self.getName()+"-->"))
+		else:
+			return self.pinA.getFrom().getOutput()
 	def getPinB(self):
-		return int(input("Enter Pin B input for gate "+ self.getLabel()+"-->"))
-
-class UnaryGate(LogicGate):
-	def __init__(self,n):
-		LogicGate.__init__(self,n)
-		self.pin = None
-	def getPin(self):
-		return int(input("Enter Pin input for gate "+ self.getLabel()+"-->"))		
+		if self.pinB == None:
+			return int(input("Enter Pin B input for gate "+self.getName()+"-->"))
+		else:
+			return self.pinB.getFrom().getOutput()
+	def setNextPin(self,source):
+		if self.pinA == None:
+			self.pinA = source
+		else:
+			if self.pinB == None:
+				self.pinB = source
+			else:
+				print("Cannot Connect: NO EMPTY PINS on this gate")
 
 class AndGate(BinaryGate):
-    def __init__(self,n):
-        BinaryGate.__init__(self,n)
-    def performGateLogic(self):
-        a = self.getPinA()
-        b = self.getPinB()
-        if a == 1 and b == 1:
-        	return 1
-        else:
-        	return 0
-
-class OrGate(BinaryGate):
 	def __init__(self,n):
 		BinaryGate.__init__(self,n)
 	def performGateLogic(self):
 		a = self.getPinA()
 		b = self.getPinB()
-		if a == 1 or b == 1:
+		if a==1 and b==1:
 			return 1
 		else:
 			return 0
+
+class OrGate(BinaryGate):
+	def __init__(self,n):
+		BinaryGate.__init__(self,n)
+
+	def performGateLogic(self):
+		a = self.getPinA()
+		b = self.getPinB()
+		if a ==1 or b==1:
+			return 1
+		else:
+			return 0
+
+class UnaryGate(LogicGate):
+	def __init__(self,n):
+		LogicGate.__init__(self,n)
+		self.pin = None
+
+	def getPin(self):
+		if self.pin == None:
+			return int(input("Enter Pin input for gate "+self.getName()+"-->"))
+		else:
+			return self.pin.getFrom().getOutput()
+
+	def setNextPin(self,source):
+		if self.pin == None:
+			self.pin = source
+		else:
+			print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
 class NotGate(UnaryGate):
 	def __init__(self,n):
 		UnaryGate.__init__(self,n)
 	def performGateLogic(self):
-		a = self.getPin()
-		if a ==1:
+		if self.getPin():
+			return 0
+		else:
+			return 1
+
+class NorGate(BinaryGate):
+	def __init__(self,n):
+		BinaryGate.__init__(self,n)
+	def performGateLogic(self):
+		a = self.getPinA()
+		b = self.getPinB()
+		if a == 0 and b == 0:
+			return 1
+		else:
+			return 0
+
+class NandGate(BinaryGate):
+	def __init__(self,n):
+		BinaryGate.__init__(self,n)
+	def performGateLogic(self):
+		a = self.getPinA()
+		b = self.getPinB()
+		if a == 1 and b == 1:
 			return 0
 		else:
 			return 1
 
 
-g1 = NotGate("G1")
-print(g1.getOutput())
+class Connector:
+    def __init__(self, fgate, tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+
+        tgate.setNextPin(self)
+
+    def getFrom(self):
+        return self.fromgate
+
+    def getTo(self):
+        return self.togate
+
+
+def main():
+   # g1 = AndGate("G1")
+   # g2 = AndGate("G2")
+   # g3 = OrGate("G3")
+   # g4 = NotGate("G4")
+   # c1 = Connector(g1,g3)
+   # c2 = Connector(g2,g3)
+   # c3 = Connector(g3,g4)
+   # print(g4.getOutput())
+
+	# g5 = nandGate("G5")
+	# print(g5.getOutput())
+	# g6 = NandGate("G6")
+	# print(g6.getOutput())
+
+# Create a series of gates that prove the following equality: 
+# NOT (( A and B) or (C and D)) is that same as NOT( A and B ) and NOT (C and D). 
+# Make sure to use some of your new gates in the simulation.
+	g1 = AndGate("G1")
+	g2 = AndGate("G2")
+	g3 = NorGate("G3")
+	c1 = Connector(g1,g3)
+	c2 = Connector(g2,g3)
+
+	g4 = NandGate("G4")
+	g5 = NandGate("G5")
+	g6 = AndGate("G6")
+	c3 = Connector(g4,g6)
+	c4 = Connector(g5,g6)
+
+	return g3.getOutput() == g6.getOutput()
+
+print(main())
